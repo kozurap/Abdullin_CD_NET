77f763461175e91d19353fb0458f779af8b7c2e5
Timur Abdullin <timur.122@yandex.ru>
2020-09-07 20:16:43 +0300

    Калькулятор

---
+++ CD_NET_HW1/CD_NET_HW1/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_NET_HW1
+{
+    class Calculator
+    {
+        int a;
+        int b;
+        string @operator;
+        public Calculator(int A,int B,string @Operator)
+        {
+            a = A;
+            b = B;
+            @operator = @Operator;
+        }
+        public int Calculate()
+        {
+            switch (@operator)
+            {
+                case "+":
+                    return (a + b);
+
+                case "-":
+                    return (a - b);
+
+                case "/":
+                    return (a / b);
+
+                case "*":
+                    return (a * b);
+
+            }
+            throw new Exception();
+        }
+    }
+}
---
+++ CD_NET_HW1/CD_NET_HW1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_NET_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            if (input.Length != 3)
+                throw new Exception("Invalid input");
+            try
+            {
+                int a = int.Parse(input[0]);
+                string mark = input[1];
+                int b = int.Parse(input[2]);
+                var calc = new Calculator(a, b, mark);
+                int res = calc.Calculate();
+                Console.WriteLine(res);
+            }
+            catch
+            {
+                throw new Exception("invalid input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
