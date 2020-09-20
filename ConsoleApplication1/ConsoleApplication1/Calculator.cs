d461eabcfa38834a07410c5151dc05bba708a3b8
Timur Abdullin <timur.122@yandex.ru>
2020-09-20 17:14:20 +0300

    Дз на 20.09.20

---
+++ ConsoleApplication1/ConsoleApplication1/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    public class Calculator
+    {
+        int a;
+        int b;
+        string @operator;
+        public Calculator(int A,int B,string @Operator)
+        {
+            a = A;
+            b = B;
+            @operator = @Operator;
+        }
+        public int Calculate()
+        {
+            switch (@operator)
+            {
+                case "+":
+                    return (a + b);
+
+                case "-":
+                    return (a - b);
+
+                case "/":
+                    return (a / b);
+
+                case "*":
+                    return (a * b);
+
+            }
+            throw new Exception("Invalid smth");
+        }    
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            if (input.Length != 3)
+                throw new Exception("Invalid input");
+            try
+            {
+                int a = int.Parse(input[0]);
+                string mark = input[1];
+                int b = int.Parse(input[2]);
+                var calc = new Calculator(a, b, mark);
+                int res = calc.Calculate();
+                Console.WriteLine(res);
+            }
+            catch
+            {
+                throw new Exception("invalid input");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
