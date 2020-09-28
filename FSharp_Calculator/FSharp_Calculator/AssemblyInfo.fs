2e2be3fed367dc5e9aa8601ffaf3d29ab4074de5
Timur Abdullin <timur.122@yandex.ru>
2020-09-28 13:12:53 +0300

    ДЗ на 28.09.2020

---
+++ FSharp_Calculator/FSharp_Calculator/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharp_Calculator.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharp_Calculator")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharp_Calculator")>]
+[<assembly: AssemblyCopyright("Copyright ©  $year$")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("C0B6C0CD-36E9-45EA-AA7B-B5B9DFEB762B")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ FSharp_Calculator/FSharp_Calculator/Program.fs
@@ -0,0 +1,57 @@
+﻿open System
+let tryParseWith(tryParseFunc:string-> bool * _) = tryParseFunc >> function
+    | true, v -> Some v
+    |false, _ -> None
+type MaybeBuilder() =
+    member this.Bind(x, f)=
+        match x with
+        |None->None
+        |Some a ->f a
+    member this.Return(x) =
+        Some x
+let maybe = new MaybeBuilder()
+let add x y = maybe{
+    let! a = x
+    let! b = y
+    return a+b
+    }
+let subtract x y =maybe{
+    let! a = x
+    let! b = y
+    return a-b
+}
+let multiply x y =maybe{
+    let! a = x
+    let! b = y
+    return a*b
+}
+let divide x y =
+    maybe
+        {
+        let! a = x
+        let! b = y
+        let! b2 =
+            match b with
+            |0.0 -> None
+            |c->Some c
+        return a/b2
+        }
+    
+let calculate op x y=
+            match op with
+            | "+" -> add x y              
+            | "-" -> subtract x y
+            | "/" -> divide x y
+            | "*" -> multiply x y
+            | _ -> None
+            
+[<EntryPoint>]
+let main args=
+    let x= Console.ReadLine()|> tryParseWith Double.TryParse
+    let op = Console.ReadLine()
+    let y = Console.ReadLine()|>tryParseWith Double.TryParse
+    let result = calculate op x y
+    match result with
+    |None-> printf"Error"
+    |Some a -> printf "%f" a
+    0
