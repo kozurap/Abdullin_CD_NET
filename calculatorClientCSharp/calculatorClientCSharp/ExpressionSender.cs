884abfd46ea42700aac0ca2a33a8869e65a8878d
Timur Abdullin <timur.122@yandex.ru>
2020-10-26 19:11:41 +0300

    HW6

---
+++ calculatorClientCSharp/calculatorClientCSharp/ExpressionSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace calculatorClientCSharp
+{
+    public static class ExpressionSender
+    {
+        public static async Task<double> GetRespAsync(string x, string y, string op)
+        {
+            Console.WriteLine("Считаю " + x + op + y);
+            if (op == "+")
+                op = "%2B";
+            HttpClient proxy = new HttpClient();
+            
+            var resp = await proxy.GetAsync("localhost:5000/?param1=" + x + "&op=" + op + "&param2=" + y);
+            var answer = resp.Headers.ToString();
+            return double.Parse(answer);
+        }
+    }
+}
---
+++ calculatorClientCSharp/calculatorClientCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace calculatorClientCSharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var a =ExpressionParser.CreateMathExpression(s);
+            var x = await Task.Run(()=>ExpressionCalcVisitor.Calculate(a));
+            Console.WriteLine("Конечный ответ: " + x);
+        }
+    }
+}
