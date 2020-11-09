c19cdfe46275619ca6bdb2550e0519217b0d8286
Timur Abdullin <timur.122@yandex.ru>
2020-11-09 16:03:38 +0300

    Have some unresolved problems

---
+++ MovieMVC/MovieMVC/Data/MVCMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieMVC.Models;
+
+namespace MovieMVC.Data
+{
+    public class MVCMovieContext : DbContext
+    {
+        public MVCMovieContext (DbContextOptions<MVCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
