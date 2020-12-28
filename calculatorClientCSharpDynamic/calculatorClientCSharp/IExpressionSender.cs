3a10ef4c3cea146a9115b2588462cf9784d374ab
Timur Abdullin <timur.122@yandex.ru>
2020-12-28 10:25:01 +0300

    deleting

--- calculatorClientCSharpDynamic/calculatorClientCSharp/IExpressionSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace calculatorClientCSharp
-{
-    public interface IExpressionSender
-    {
-        public Task<double> GetRespAsync(double x, double y, string op);
-    }
-}
